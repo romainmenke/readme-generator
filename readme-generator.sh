d3c38cd57aed17e2259be92b5bd19643562a88e1
romainmenke <romainmenke@gmail.com>
2016-10-05 14:20:45 +0200

    improvements

--- readme-generator.sh
+++ readme-generator.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-cd $1
+PROJECTPATH=$GOPATH/src/$1
 
-> $1/README_INDEX.md
+cd $PROJECTPATH
+
+> $PROJECTPATH/README_INDEX.md
 
 FILES=$(find . -name 'README.md')
 
 for f in $FILES
 do
-	MASTER=$1"/tree/master"
+	MASTER=$PROJECTPATH"/tree/master"
 	TEMP=${f/\./$MASTER}
 	REMOVE=$GOPATH\/src
 	CLEAN=${TEMP/$REMOVE/"https:/"}
 	CLEAN=${CLEAN/README.md/""}
 
-	echo $CLEAN >> $1/README_INDEX.md
-	echo "" >> $1/README_INDEX.md
+	echo $CLEAN >> $PROJECTPATH/README_INDEX.md
+	echo "" >> $PROJECTPATH/README_INDEX.md
 done
