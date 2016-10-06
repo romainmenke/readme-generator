27482c43e3bddbb50d707d68ab4c8de9a76e38fe
romainmenke <romainmenke@gmail.com>
2016-10-06 09:56:54 +0200

    without vendoring

--- readme-generator.sh
+++ readme-generator.sh
@@ -10,12 +10,25 @@ FILES=$(find . -name 'README.md')
 
 for f in $FILES
 do
-	MASTER=$PROJECTPATH"/tree/master"
-	TEMP=${f/\./$MASTER}
-	REMOVE=$GOPATH\/src
-	CLEAN=${TEMP/$REMOVE/"https:/"}
-	CLEAN=${CLEAN/README.md/""}
 
-	echo $CLEAN >> $PROJECTPATH/README_INDEX.md
-	echo "" >> $PROJECTPATH/README_INDEX.md
+	BASE_DIRECTORY=$(echo "$f" | cut -d "/" -f2)
+
+	if [ $BASE_DIRECTORY != "vendor" ]; then
+
+		MASTER=$PROJECTPATH"/tree/master"
+		TEMP=${f/\./$MASTER}
+		REMOVE=$GOPATH\/src
+		CLEAN=${TEMP/$REMOVE/"https:/"}
+		CLEAN=${CLEAN/README.md/""}
+		NAME=$(basename $CLEAN)
+		PARENT=$(basename $(dirname $CLEAN))
+
+		if [ $NAME != "master" ]; then
+
+			echo \[$PARENT/$NAME\]\($CLEAN\) >> $PROJECTPATH/README_INDEX.md
+			echo "" >> $PROJECTPATH/README_INDEX.md
+
+		fi
+
+	fi
 done
